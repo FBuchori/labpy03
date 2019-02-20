5d0470a40a048568927cc18b8b4f0912de5c0812
FBuchori <45659267+FBuchori@users.noreply.github.com>
2019-02-20 21:11:41 +0700

    Upload Gambar

---
+++ Latihan2.py
@@ -0,0 +1,10 @@
+print ("-------LATIHAN 2------")
+print ("Menampilkan Bilangan, Berhenti Ketika Bilangan 0, dan Menampilkan Bilangan Terbesar")
+
+b=0
+while True:
+    angka=int(input("Masukkan Bilangan: "))
+    b=angka
+    if b == 0:
+        break
+print ("---------DONE----------")
